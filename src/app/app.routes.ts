2157ef159cbe456007e4b167f70b5635efb15d56
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-06-04 22:02:26 +0500

    basic routing with layouts is added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,35 @@ import { Routes } from '@angular/router';
 import { authRoutes } from './pages/auth/routes';
 import { authGuard } from './core/guards/auth.guard';
 import { HomeComponent, NotFoundPageComponent } from './pages';
+import { DefaultLayoutComponent, EmptyLayoutComponent } from './layouts';
+
+const emptyLayoutRoutes: Routes = [
+  ...authRoutes,
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
+
+const defaultLayoutRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
 
 export const routes: Routes = [
-    ...authRoutes,
-    {
-        path: '',
-        component: HomeComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: "**",
-        component: NotFoundPageComponent
-    }
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    canActivateChild: [authGuard],
+    canMatch: [authGuard],
+    children: [...defaultLayoutRoutes],
+  },
+  {
+    path: '',
+    component: EmptyLayoutComponent,
+    children: [...emptyLayoutRoutes],
+  },
 ];
