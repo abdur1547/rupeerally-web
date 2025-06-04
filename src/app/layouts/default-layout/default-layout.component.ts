32e8c5ba106225c3af3f2fff5f8e698c0682c099
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-06-04 20:51:12 +0500

    renamed to add layout

---
+++ src/app/layouts/default-layout/default-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'default-layout',
+  imports: [],
+  templateUrl: './default-layout.component.html',
+  styleUrl: './default-layout.component.scss',
+})
+export class DefaultLayoutComponent {}
